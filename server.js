5ae98497098ed1c87658ebf126f7e83a8a69764a
Berry Nieskens <berrynieskens@outlook.com>
2024-02-12 21:03:09 +0100

    dog api

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.static("static"))
 
 app.get('/helloworld', (req, res) => {
-  res.send('Hello World!!!')
+  res.send('Hello World!')
 })
 
 app.listen(3000, () => console.log("Server running on port 3000"))
--- static/script.js
+++ static/script.js
@@ -1,2 +1,22 @@
-console.log("Hello client!")
 
+console.log("Hello client!!!")
+
+/* DOG IMAGE API ************/
+/* https://dog.ceo/dog-api/ */
+/****************************/
+
+const imgRandomDog = document.querySelector("#imgRandomDog")
+const btnRandomDog = document.querySelector("#btnRandomDog")
+
+async function getRandomDogImage() {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random')
+    const data = await response.json()
+    return data.message
+}
+
+async function setNewDogImage(){
+    const image = await getRandomDogImage()
+    imgRandomDog.src = `${image}`
+}
+
+btnRandomDog.addEventListener('click', setNewDogImage)

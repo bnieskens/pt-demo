0ae0760b1caba31439600e918c6a15f009016c35
Berry Nieskens <berrynieskens@outlook.com>
2024-02-13 22:02:42 +0100

    Mariibo loader 0.1

--- static/script.js
+++ static/script.js
@@ -25,7 +25,7 @@ btnRandomDog.addEventListener('click', setNewDogImage)
 /* MARIIBO'S *****************/
 /* https://www.amiiboapi.com */
 /*****************************/
-// api documentation: 
+
 const baseURL = "https://www.amiiboapi.com/api/";
 const endPoint = "amiibo/?gameseries=Super Mario";
 const URL = baseURL+endPoint;
@@ -57,3 +57,44 @@ async function setAmiibo(){
 
 }
 btnMariibo.addEventListener('click', setAmiibo)
+
+
+/* MARIIBO'S *****************/
+/* Empty & loading state *****/
+/* https://www.amiiboapi.com */
+/*****************************/
+const loader = document.querySelector('#loaderDiv');
+
+const listLoader = document.querySelector('#ulMariiboLoader');
+const btnMariiboLoader = document.querySelector('#btnMariiboLoader');
+
+
+async function getAmiiboLoader() {
+    await new Promise(resolve => setTimeout(resolve, 3000)) // GPT
+	const response = await fetch(URL)
+	const data = await response.json()
+    return data.amiibo
+}
+
+async function setAmiiboLoader(){
+    loader.classList.add("loading")
+    console.log("BTN loader clicked")
+    amiibo = await getAmiiboLoader()
+    loader.classList.remove("loading")
+    amiibo.forEach((amiibo) => {
+        const amiiboListElement = document.createElement("li")
+        amiiboListElement.innerHTML = 
+            `
+            <li>
+                <h3>${amiibo.character}</h3>
+                <img src="${amiibo.image}" alt="${amiibo.character}" width=100>
+            </li>
+            `
+        listLoader.appendChild(amiiboListElement)
+    })
+
+}
+
+
+
+btnMariiboLoader.addEventListener('click', setAmiiboLoader)

8e1f41cd5d9f1d4b50c4af381e5c860e3264bee7
Berry Nieskens <b.nieskens@hva.nl>
2024-02-16 11:59:43 +0100

    update ListJS

--- static/demo2/script.js
+++ static/demo2/script.js
@@ -1,62 +1,7 @@
 console.log("Hello client!");
 
-/* DOG IMAGE API ************/
-/* https://dog.ceo/dog-api/ */
-/****************************/
-
-const imgRandomDog = document.querySelector("#imgRandomDog");
-const btnRandomDog = document.querySelector("#btnRandomDog");
-
-async function getRandomDogImage() {
-  const response = await fetch("https://dog.ceo/api/breeds/image/random");
-  const data = await response.json();
-  console.log("Hond data:" + Object.keys(data));
-  console.log("Hond data:" + data.message);
-
-  return data.message;
-}
-
-async function setNewDogImage() {
-  const image = await getRandomDogImage();
-  imgRandomDog.src = `${image}`;
-}
-
-btnRandomDog.addEventListener("click", setNewDogImage);
-
 /* MARIIBO'S *****************/
-/* https://www.amiiboapi.com */
-/*****************************/
-
-const baseURL = "https://www.amiiboapi.com/api/";
-const endPoint = "amiibo/?gameseries=Super Mario";
-const URL = baseURL + endPoint;
-
-const list = document.querySelector("#ulMariibo");
-const btnMariibo = document.querySelector("#btnMariibo");
-
-async function getAmiibo() {
-  const response = await fetch(URL);
-  const data = await response.json();
-  console.log(typeof data.amiibo); // typeof: object? (Array)
-  return data.amiibo;
-}
-
-async function renderAmiibo() {
-  const amiibos = await getAmiibo();
-
-  amiibos.forEach((amiibo) => {
-    const amiiboListElement = document.createElement("li");
-    amiiboListElement.innerHTML = `
-                <h3>${amiibo.character}</h3>
-                <img src="${amiibo.image}" alt="${amiibo.character}" width=100>
-            `;
-    list.appendChild(amiiboListElement);
-  });
-}
-btnMariibo.addEventListener("click", renderAmiibo);
-
-/* MARIIBO'S *****************/
-/* Empty & loading state *****/
+/* LISTJS zoekfunctie *****/
 /* https://www.amiiboapi.com */
 /*****************************/
 const loader = document.querySelector("#loaderDiv");
@@ -65,7 +10,7 @@ const listLoader = document.querySelector("#ulMariiboLoader");
 const btnMariiboLoader = document.querySelector("#btnMariiboLoader");
 
 async function getAmiiboLoader() {
-  await new Promise((resolve) => setTimeout(resolve, 1000)); // GPT
+  // await new Promise((resolve) => setTimeout(resolve, 1000)); // GPT
   const response = await fetch(URL);
   const data = await response.json();
   return data.amiibo;
@@ -80,9 +25,9 @@ async function setAmiiboLoader() {
     const amiiboListElement = document.createElement("li");
     amiiboListElement.innerHTML = `
             <li>
-                <h4>${amiibo.character}</h3>
+                <h4 class="name">${amiibo.character}</h3>
                 <img src="${amiibo.image}" alt="${amiibo.character}" height=100>
-                <p>${amiibo.type}</p>
+                <p class="type">${amiibo.type}</p>
             </li>
             `;
     listLoader.appendChild(amiiboListElement);
@@ -90,3 +35,13 @@ async function setAmiiboLoader() {
 }
 
 btnMariiboLoader.addEventListener("click", setAmiiboLoader);
+
+// ListJS
+
+const options = {
+  valueNames: ["name", "movie"]
+}
+
+const testList = new List("MariiboLoader", options)
+console.log(typeof testList, testList.add)
+

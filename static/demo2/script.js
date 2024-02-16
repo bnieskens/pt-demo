bece8376dfc33d6163a8c9455e2e971e7c230aa5
Berry Nieskens <b.nieskens@hva.nl>
2024-02-16 11:00:39 +0100

    verdeling over pagina's

--- static/demo2/script.js
+++ static/demo2/script.js
@@ -1,58 +1,5 @@
 console.log("Hello client!");
 
-/* DOG IMAGE API ************/
-/* https://dog.ceo/dog-api/ */
-/****************************/
-
-const imgRandomDog = document.querySelector("#imgRandomDog");
-const btnRandomDog = document.querySelector("#btnRandomDog");
-
-async function getRandomDogImage() {
-  const response = await fetch("https://dog.ceo/api/breeds/image/random");
-  const data = await response.json();
-  return data.message;
-}
-
-async function setNewDogImage() {
-  const image = await getRandomDogImage();
-  imgRandomDog.src = `${image}`;
-}
-
-btnRandomDog.addEventListener("click", setNewDogImage);
-
-/* MARIIBO'S *****************/
-/* https://www.amiiboapi.com */
-/*****************************/
-
-const baseURL = "https://www.amiiboapi.com/api/";
-const endPoint = "amiibo/?gameseries=Super Mario";
-const URL = baseURL + endPoint;
-
-const list = document.querySelector("#ulMariibo");
-const btnMariibo = document.querySelector("#btnMariibo");
-
-async function getAmiibo() {
-  const response = await fetch(URL);
-  const data = await response.json();
-  return data.amiibo;
-}
-
-async function setAmiibo() {
-  amiibo = await getAmiibo();
-
-  amiibo.forEach((amiibo) => {
-    const amiiboListElement = document.createElement("li");
-    amiiboListElement.innerHTML = `
-            <li>
-                <h3>${amiibo.character}</h3>
-                <img src="${amiibo.image}" alt="${amiibo.character}" width=100>
-            </li>
-            `;
-    list.appendChild(amiiboListElement);
-  });
-}
-btnMariibo.addEventListener("click", setAmiibo);
-
 /* MARIIBO'S *****************/
 /* Empty & loading state *****/
 /* https://www.amiiboapi.com */
@@ -77,11 +24,9 @@ async function setAmiiboLoader() {
   amiibo.forEach((amiibo) => {
     const amiiboListElement = document.createElement("li");
     amiiboListElement.innerHTML = `
-            <li>
-                <h4>${amiibo.character}</h3>
+                <h4>${amiibo.character}</h4>
                 <img src="${amiibo.image}" alt="${amiibo.character}" height=100>
                 <p>${amiibo.type}</p>
-            </li>
             `;
     listLoader.appendChild(amiiboListElement);
   });
